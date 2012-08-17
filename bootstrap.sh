d679dd8380455e31c4aa213139bce0c25ba40b47
Terje Larsen <terlar@gmail.com>
2012-08-17 14:13:03 +0200

    Specifies vim path in a variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
+vim_path="$HOME/.vim"
 
 # Remove and backup old files
-if [ -e $HOME/.vim ]; then
-  cp -r $HOME/.vim $HOME/.vim.old
-  rm -rf $HOME/.vim
+if [ -e $vim_path ]; then
+  cp -r $vim_path "$vim_path.old"
+  rm -rf $vim_path
 fi
-if [ -e $HOME/.vimrc ]; then
-  cp $HOME/.vimrc $HOME/.vimrc.old
-  rm -f $HOME/.vimrc
+if [ -e "$HOME/.vimrc" ]; then
+  cp "$HOME/.vimrc" "$HOME/.vimrc.old"
+  rm -f "$HOME/.vimrc"
 fi
 
 # Steup new vim config
-git clone https://github.com/terlar/vimfiles $HOME/.vim
-ln -s $HOME/.vim/vimrc $HOME/.vimrc
+git clone https://github.com/terlar/vimfiles $vim_path
+ln -s "$vim_path/vimrc" "$HOME/.vimrc"
 
 # Install bundles
-vim -c BundleInstall! -c qa -u $HOME/.vim/config/bundles.vim
+vim -c BundleInstall! -c qa -u $vim_path/config/bundles.vim
 
 # Create dir for undo history
-mkdir -p $HOME/.vimundo
+mkdir -p "$HOME/.vimundo"
