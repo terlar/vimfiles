6d92bbe5db48b2f089ea69c2fefa452cc58617c2
Terje Larsen <terlar@gmail.com>
2012-08-17 14:25:33 +0200

    Do things in the right order

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,13 @@
 vim_path="$HOME/.vim"
 
 # Remove and backup old files
-if [ -e $vim_path ]; then
-  cp -r $vim_path "$vim_path.old"
-  rm -rf $vim_path
-fi
 if [ -e "$HOME/.vimrc" ]; then
   cp "$HOME/.vimrc" "$HOME/.vimrc.old"
   rm -f "$HOME/.vimrc"
+fi
+if [ -e $vim_path ]; then
+  cp -r $vim_path "$vim_path.old"
+  rm -rf $vim_path
 fi
 
 # Steup new vim config
